c446840df1dc5cc7a8e3a981193e2fb809622c5e
Matej Suta <suta.matej@gmail.com>
2012-12-11 21:01:05 +0100

    modified mac deployment script

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 cp -vR \
-.gitconfig \
-.vim \
-.vimrc \
+mac/ \
 ~
 
-echo "Please, run import one of the profiles located in .themes/mac to your terminal"
+#echo "Please, run import one of the profiles located in .themes/mac to your terminal"
 
